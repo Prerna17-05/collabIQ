eaff48bdf92f95187177b9a2f59a5af84bededc9
Prerna17-05 <prernadongredongre@gmail.com>
2026-03-12 17:17:45 +0530

    Create riskNormalizer.ts

---
+++ collabiq/services/risk-engine/riskNormalizer.ts
@@ -0,0 +1,5 @@
+export function normalize(value: number, min: number, max: number): number {
+  if (value < min) return 0;
+  if (value > max) return 100;
+  return ((value - min) / (max - min)) * 100;
+}
